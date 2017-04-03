168f39a209d9090a0a9bfd1c04c90b796e55f67c
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2017-04-03 11:06:21 +0200

    Split command

--- .bin/tig-rebase.sh
+++ .bin/tig-rebase.sh
@@ -6,7 +6,7 @@ commit=$2
 
 function usage()
 {
-    echo "usage: $program fixup|squash|ascend|descend|reword|abort|edit|move|drop HASH"
+    echo "usage: $program fixup|squash|ascend|descend|reword|abort|edit|move|drop|split HASH"
     exit 1
 }
 
@@ -14,8 +14,24 @@ function usage()
 
 replace="gsed -i"
 
+function shell_rebase_continue () {
+    while true; do
+        PROMPT_PREFIX="<tig $action $current> " zsh -i
+        git status
+        if [ -n "$(git status --porcelain)" ]; then
+            read -n 1 -p "Working directory is not clean. git rebase --continue [y/n/a]? " X
+            test "$X" = "a" && { git rebase --abort; exit 1; }
+            test "$X" != "y" && continue
+        fi
+        git rebase --continue && break
+        printf '\a'
+        read -n 1 -p "Abort [y/n]? " X
+        test "$X" = "y" && { git rebase --abort; exit 1; }
+    done
+}
+
 case $action in
-    fixup|squash|ascend|descend|reword|abort|edit|move|drop)
+    fixup|squash|ascend|descend|reword|abort|edit|move|drop|split)
         if [ "$action" != "abort" ]; then
             if [ -z $commit ]; then
                 usage
@@ -38,8 +54,13 @@ case $action in
             edit)
                 replace="$replace -e 's/pick ${current}/e ${current}/'"
                 GIT_SEQUENCE_EDITOR=$replace git rebase -i ${current}~2
-                PROMPT_PREFIX="<tig edit> " zsh -i
-                git rebase --continue
+                shell_rebase_continue
+                ;;
+            split)
+                replace="$replace -e 's/pick ${current}/e ${current}/'"
+                GIT_SEQUENCE_EDITOR=$replace git rebase -i ${current}~2
+                git reset "HEAD^"
+                shell_rebase_continue
                 ;;
             squash)
                 replace="$replace -e 's/pick ${current}/s ${current}/'"
@@ -52,19 +73,19 @@ case $action in
                 test "$(git log --pretty=format:%h $(git merge-base HEAD "${mark}") -1)" = "${mark}" || exit 0
                 replace="$replace -e 's/pick ${mark} .*/noop/;s/pick ${current} .*/pick ${current}\\npick ${mark}\\nx git tag -f mark/'"
                 base=$(git merge-base ${current} ${mark})
-                GIT_SEQUENCE_EDITOR="$replace" git rebase -i ${base}~2 || (printf '\a'; git rebase --abort; exit 1)
+                GIT_SEQUENCE_EDITOR="$replace" git rebase -i ${base}~2 || { printf '\a'; git status; git rebase --abort; exit 1; }
                 ;;
             ascend)
                 replace="$replace -e 's/pick ${current}/pick BUFFER/'"
                 replace="$replace -e 's/pick ${child}/pick ${current}/'"
                 replace="$replace -e 's/pick BUFFER/pick ${child}/'"
-                GIT_SEQUENCE_EDITOR=$replace git rebase -i ${current}~1 || (printf '\a'; git rebase --abort; exit 1)
+                GIT_SEQUENCE_EDITOR=$replace git rebase -i ${current}~1 || { printf '\a'; git status; git rebase --abort; exit 1; }
                 ;;
             descend)
                 replace="$replace -e 's/pick ${current}/pick BUFFER/'"
                 replace="$replace -e 's/pick ${parent}/pick ${current}/'"
                 replace="$replace -e 's/pick BUFFER/pick ${parent}/'"
-                GIT_SEQUENCE_EDITOR=$replace git rebase -i ${current}~2 || (printf '\a'; git rebase --abort; exit 1)
+                GIT_SEQUENCE_EDITOR=$replace git rebase -i ${current}~2 || { printf '\a'; git status; git rebase --abort; exit 1; }
                 ;;
             reword)
                 replace="$replace -e 's/pick ${current}/reword ${current}/'"
